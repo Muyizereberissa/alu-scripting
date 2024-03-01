d42d77ad3c220b4c78ea75b706cb83db6c5cfc7b
Muyizereberissa <muyizereberissa22@gmail.com>
2024-03-01 05:52:47 +0000

    script

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-function that queries the 'Reddit API' and returns the number of subscribers
-"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """
-    number of subscribers
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    
+    response = requests.get(url, headers=headers)
+    
     if response.status_code == 200:
-        data = response.json()["data"]
-        return data["subscribers"]
+        data = response.json()
+        return data['data']['subscribers']
     else:
         return 0
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
