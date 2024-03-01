82a9e6d8a5dbf4ce8596a5dcd725a135000cadf5
Muyizereberissa <muyizereberissa22@gmail.com>
2024-03-01 06:12:50 +0000

    script

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
-"""Return number of subscribers for a given subreddit"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers """
-    url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers)
+    
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    
     if response.status_code == 200:
-        return response.json().get('data') \
-            .get('subscribers')
-    return 0
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
